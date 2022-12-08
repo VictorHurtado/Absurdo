838986c155eddb96b5142dd913f2a428bb5bd922
VictorHurtado <victor2012lopezxd@gmail.com>
2022-12-08 13:32:00 -0500

    Creación del sidebar de la tienda

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,34 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import {ShopLayout} from '../components/layouts';
-import { Typography } from '@mui/material';
-
+import { ShopLayout } from '../components/layouts';
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
+import { initialData } from '../database/products';
+initialData
 export default function Home() {
   return (
-   <ShopLayout title={'Absurdo'} pageDescription={'Encuentra cosas absurdas'}>
-    <Typography variant='h1' component={'h1'}> Tienda </Typography>
-    <Typography variant='h2' style={{ marginBottom:1 }}> Tienda </Typography>
-   </ShopLayout>
+    <ShopLayout title={'Absurdo'} pageDescription={'Encuentra cosas absurdas'}>
+      <Typography variant='h1' component={'h1'}> Tienda </Typography>
+      <Typography variant='h2' style={{ marginBottom: 1 }}> Todos los productos </Typography>
+      <Grid container spacing={4}>
+
+        {
+          initialData.products.map(product => (
+            <Grid item xs={6} sm={4} key={product.slug}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component={'img'}
+                    image={`products/${product.images[0]}`}
+                    alt={product.type}
+                  />
+                </CardActionArea>                
+              </Card>
+            </Grid>
+          ))
+        }
+
+      </Grid>
+    </ShopLayout>
   )
 }
