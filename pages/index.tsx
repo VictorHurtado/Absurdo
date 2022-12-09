c9ac8dc226e3cbb326ed2fa74438174d0945e36f
VictorHurtado <victor2012lopezxd@gmail.com>
2022-12-09 12:10:54 -0500

    Creación de product page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { ShopLayout } from '../components/layouts';
-import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import { initialData } from '../database/products';
+import ProductList from '../components/products/ProductList';
 initialData
 export default function Home() {
   return (
     <ShopLayout title={'Absurdo'} pageDescription={'Encuentra cosas absurdas'}>
       <Typography variant='h1' component={'h1'}> Tienda </Typography>
       <Typography variant='h2' style={{ marginBottom: 1 }}> Todos los productos </Typography>
-      <Grid container spacing={4}>
-
-        {
-          initialData.products.map(product => (
-            <Grid item xs={6} sm={4} key={product.slug}>
-              <Card>
-                <CardActionArea>
-                  <CardMedia
-                    component={'img'}
-                    image={`products/${product.images[0]}`}
-                    alt={product.type}
-                  />
-                </CardActionArea>                
-              </Card>
-            </Grid>
-          ))
-        }
-
-      </Grid>
+      <ProductList
+        products={initialData.products as any}
+      />
     </ShopLayout>
   )
 }
